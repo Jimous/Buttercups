f2540474115c7d6b080e5c640559a2a76f428101
Jimous <jandresvd@gmail.com>
2016-05-03 10:44:03 +0200

    Delete readSensors.m
    Function was moved to GUI

--- readSensors.m
+++
@@ -1,8 +0,0 @@
-function [ sensorValue ] = readSensors( serial )
-% 'readSensors' saves a sensor reading into array as float values.
-% By Jimmy Andres Vargas Delgado - April 7, 2016.
-fprintf(serial,'%s',sprintf('1;'));
-inputString = fscanf(serial,'%s');%example: inputString = '0.3234;890.2344;373.3412';
-inputString = strsplit(inputString,';');
-sensorValue = str2double(inputString);
-end
